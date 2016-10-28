bf02cf58c44b3a3305fccae91bd14ab24c52b592
sujith9 <sujithmahendrakar9@gmail.com>
2016-10-28 12:51:08 -0400

    removed duplicate unique constraint

--- scripts/psych.sql
+++ scripts/psych.sql
@@ -361,6 +361,3 @@ CREATE TABLE `imageResponse` (
   ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 ##########################################################################
-
-
-ALTER TABLE `fieldLookup` ADD UNIQUE `unique_index`(`fieldName`, `attribute`)
