5401f97ff7731284508b0d764f139beb7d535ee3
Darshan Patel <patel.dars@husky.neu.edu>
2016-11-08 19:42:05 -0500

    location missing file added

---
+++ src/common/Location.java
@@ -0,0 +1,93 @@
+package common;
+
+public class Location {
+	
+	String name;
+	String desc;
+	String keywords;
+	String code;
+	String addressLine1;
+	String addressLine2;
+	String city;
+	Long state;
+	Long zipCode;
+	Long phoneNumber;
+	Long faxNumber;
+	String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Long getState() {
+		return state;
+	}
+	public void setState(Long state) {
+		this.state = state;
+	}
+	public Long getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(Long zipCode) {
+		this.zipCode = zipCode;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Long getFaxNumber() {
+		return faxNumber;
+	}
+	public void setFaxNumber(Long faxNumber) {
+		this.faxNumber = faxNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+	
+}
