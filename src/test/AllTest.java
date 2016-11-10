6213c8437d2708a1302f0165f60678926c30f037
Darshan Patel <patel.dars@husky.neu.edu>
2016-11-10 02:13:44 -0500

    Code coverage library added

--- src/test/AllTest.java
+++ src/test/AllTest.java
@@ -7,9 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import authentication.AdminAuthenticationServletTest;
 import autoFill.FetchCommonFieldServletTest;
 import fieldValidation.LocationFieldsTest;
+import fieldValidation.QuestionCategoryFieldsValTest;
 import fieldValidation.UserProfileFieldsTest;
 import location.LocationServletTest;
 import profile.UserProfileServletTest;
+import questionnaire.QuestionCategoryServlet;
+import questionnaire.QuestionCategoryServletTest;
+import questionnaire.QuestionServletTest;
+import questionnaire.QuestionnaireTest;
 import targetGroup.TargetGroupServletTest;
 import training.TrainingServletTest;
 
@@ -22,7 +27,9 @@ import training.TrainingServletTest;
 	LocationFieldsTest.class,
 	LocationServletTest.class,
 	TargetGroupServletTest.class,
-	TrainingServletTest.class
+	TrainingServletTest.class,
+	QuestionCategoryFieldsValTest.class,
+	QuestionCategoryServletTest.class
 })
 public class AllTest {
 
