7e56299686854a573a62b869dd0a0c50135e5cb9
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-11-23 22:53:10 -0500

    testing image upload

--- WebContent/config.js
+++ WebContent/config.js
@@ -8,6 +8,21 @@
     angular
         .module("PsychWebApp")
         .constant("serverURL", { url : 'http://localhost:8080/Psych-1/'})
+        .directive('fileUpload', function () {
+        	return {
+        		scope: true,        //create a new scope
+        		link: function (scope, el, attrs) {
+        			el.bind('change', function (event) {
+	                var files = event.target.files;
+	                //iterate files since 'multiple' may be specified on the element
+	                for (var i = 0;i<files.length;i++) {
+	                    //emit event upward
+	                    scope.$emit("fileSelected", { file: files[i] });
+                }                                       
+            });
+        }
+    };
+        })
         .config(function($routeProvider, $httpProvider)
         {
             $routeProvider
