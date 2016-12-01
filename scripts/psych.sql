37dd41345b2d3b0ed73cb5390529798544d02363
Darshan Patel <darshandpatel@users.noreply.github.com>
2016-11-30 20:39:32 -0500

    PC-50 background table added

--- scripts/psych.sql
+++ scripts/psych.sql
@@ -204,6 +204,19 @@ CREATE TABLE `question` (
 
 ##########################################################################
 
+DROP TABLE IF EXISTS `background`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `background` (
+  `backgroundId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `bgColor` varchar(200) NOT NULL,
+  PRIMARY KEY (`backgroundId`),
+  UNIQUE KEY `bgColor` (`bgColor`)
+) ENGINE=InnoDB AUTO_INCREMENT=965 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+##########################################################################
+
 /*imageCategory table*/
 
 DROP TABLE IF EXISTS `imageCategory`;
